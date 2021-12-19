bab8b60c802ae00a08f39c29f3201d662d2ae2b7
Asilbec <asilbekomonkulov2003@gmail.com>
2021-12-18 22:55:16 -0500

    added round feature

--- src/App.js
+++ src/App.js
@@ -6,33 +6,69 @@ import {
   withScriptjs,
   GoogleMap,
   Marker,
-  StreetViewPanorama
+  StreetViewPanorama,
+  Polyline,
 } from "react-google-maps";
 
-function Map() {
-  const [marker, newMarker] = useState()
+function Map(props) {
+  const [marker, newMarker] = useState({ lat: 0, lng: 0 })
   const OPTIONS = {
-    minZoom: 4,
+    minZoom: 2,
     maxZoom: 18,
+    streetViewControl: false,
+    fullscreenControl: false
   }
+  const path = [
+    { lat: props.lat, lng: props.lng },
+    { lat: marker.lat, lng: marker.lng }
+  ];
+
+  const option = {
+    geodesic: true,
+    strokeColor: "#FF0000",
+    strokeOpacity: 1.0,
+    strokeWeight: 2,
+  }
+  let iconMarker = new window.google.maps.MarkerImage(
+    "https://www.freeiconspng.com/uploads/pushpin-png-13.png",
+    null, /* size is determined at runtime */
+    null, /* origin is 0,0 */
+    null, /* anchor is bottom center of the scaled image */
+    new window.google.maps.Size(32, 32)
+  );
+  let iconMarker2 = new window.google.maps.MarkerImage(
+    "https://www.freepnglogos.com/uploads/target-png/target-logo-png-transparent-svg-vector-bie-supply-35.png",
+    null, /* size is determined at runtime */
+    null, /* origin is 0,0 */
+    null, /* anchor is bottom center of the scaled image */
+    new window.google.maps.Size(32, 32)
+  );
   return (
     <GoogleMap
       defaultZoom={4}
       options={OPTIONS}
       defaultCenter={{ lat: 45.4211, lng: -75.6903 }}
       onClick={(event) => {
-        document.getElementById('lat').innerHTML = event.latLng.lat()
-        document.getElementById('lng').innerHTML = event.latLng.lng()
-        newMarker({ lat: event.latLng.lat(), lng: event.latLng.lng() })
-      }}><Marker position={marker} />
+        if (props.newMarkerStuff === true) {
+          document.getElementById('lat').innerHTML = event.latLng.lat()
+          document.getElementById('lng').innerHTML = event.latLng.lng()
+          newMarker({ lat: event.latLng.lat(), lng: event.latLng.lng() })
+        }
+      }}><Marker position={marker} icon={iconMarker}></Marker>
+      <Marker icon={iconMarker2} visible={props.state}
+        position={{ lat: props.lat, lng: props.lng }} />
+      <Polyline options={option} visible={props.state} path={path}>
+      </Polyline>
     </GoogleMap>);
 }
 function streetview(props) {
   const mapOptions = {
     disableDefaultUI: true,
     enableCloseButton: false,
-    showRoadLabels: false
+    showRoadLabels: false,
+    motionTracking: false
   };
+
   return (
     <GoogleMap
       defaultCenter={{ lat: 40.403154441461346, lng: -80.06076967448335 }}
@@ -52,27 +88,61 @@ function streetview(props) {
     </GoogleMap>
   )
 }
-
 const MapWrapped = withScriptjs(withGoogleMap(Map));
 const StreetView = withScriptjs(withGoogleMap(streetview));
+
+
+
 function App() {
+
+  const [round, newRound] = useState(0)
+  const [markerState, newMarkerState] = useState(false)
+  const [markerStatus, newmarkerStatus] = useState(true)
+  const [points, newPoints] = useState({
+    lat: 40.403154441461346, lng: -80.06076967448335
+  })
 
   function newArea() {
     const lat1 = document.getElementById('lat').innerHTML
     const lng1 = document.getElementById('lng').innerHTML
     const lat2 = points.lat
     const lng2 = points.lng
-
     console.log("difference is " + calcCrow(lat1, lng1, lat2, lng2))
-    document.getElementById('differnce').innerHTML = calcCrow(lat1, lng1, lat2, lng2)
-    newview()
+    document.getElementById('nice').style.width = '100vw'
+    document.getElementById('nice').style.height = '100vh'
+    document.getElementById('nice').style.padding = '0px'
+    document.getElementById('submitButton').style.display = 'none'
+    document.getElementById('infoshown').style.display = 'flex'
+    newMarkerState(true)
+    document.getElementById('totalDifference').innerText = 'Total difference is ' + (calcCrow(lat1, lng1, lat2, lng2)).toFixed(2)
+    newmarkerStatus(false)
+    setTimeout(() => {
+      document.getElementById('nice').style.width = '20vw'
+      document.getElementById('nice').style.height = '20vw'
+      document.getElementById('submitButton').style.display = 'inline'
+      document.getElementById('infoshown').style.display = 'none'
+      document.getElementById('nice').style.padding = '2px'
+      newmarkerStatus(true)
+      newMarkerState(false)
+      newview()
+    }, 10000);
   }
 
 
+  function changeWidth() {
+    if (markerState === false) {
+      document.getElementById('nice').style.width = '30vw'
+      document.getElementById('nice').style.height = '30vw'
+    }
+  }
 
-  const [points, newPoints] = useState({
-    lat: 40.403154441461346, lng: -80.06076967448335
-  })
+  function changeBack() {
+    if (markerState === false) {
+      document.getElementById('nice').style.width = '20vw'
+      document.getElementById('nice').style.height = '20vw'
+    }
+  }
+
 
   function calcCrow(lat1, lon1, lat2, lon2) {
     var R = 6371; // km
@@ -80,7 +150,6 @@ function App() {
     var dLon = toRad(lon2 - lon1);
     var latOne = toRad(lat1);
     var latTwo = toRad(lat2);
-
     var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
       Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(latOne) * Math.cos(latTwo);
     var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
@@ -88,35 +157,29 @@ function App() {
     return d;
   }
 
+
   function newview() {
     randomStreetView.getRandomLocations(1).then(function (response) {
-      console.log("lat: " + response[0][0] + " lng : " + response[0][1])
       newPoints({
         lat: response[0][0],
         lng: response[0][1]
       })
+      newRound(round + 1)
     })
   }
   // Converts numeric degrees to radians
   function toRad(Value) {
     return Value * Math.PI / 180;
   }
+
   useEffect(() => {
     newview()
-
   }, []);
   return (
     <div className='App'>
-      <div className='game'>
-        <div className="map" >
-          <MapWrapped
-            googleMapURL={"https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyCea_aNWrtFKwa63zn0e3xpkpBTe2QYAFU"}
-            loadingElement={<div className='loadingStuff' />}
-            containerElement={<div className='loadingStuff' />}
-            mapElement={<div className='loadingStuff' />}
-          />
-        </div>
-        <div className='section'>
+      <div className='game' id='games'>
+        <div className='roundcounter'>Round : {round}</div>
+        <div className='mapFunction'>
           <div className="map">
             <StreetView
               lat={points.lat}
@@ -127,21 +190,29 @@ function App() {
               mapElement={<div className='loadingStuff' />}
             />
           </div>
-          <div className='infoContainer'>
-            <button onClick={() => newArea()} id='submit'></button>
-            <div className='textinfo'>
-
-              <p id='differnce'>the difference is : </p>
-            </div>
-            <div className='hidden'>
-              <h1 id='lat'>?</h1>
-              <h1 id='lng'>?</h1>
-            </div>
+        </div>
+        <div className="map" onMouseLeave={() => changeBack()} onMouseEnter={() => changeWidth()} id='nice' >
+          <MapWrapped
+            id='streetviewmap'
+            newMarkerStuff={markerStatus}
+            googleMapURL={"https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyCea_aNWrtFKwa63zn0e3xpkpBTe2QYAFU"}
+            loadingElement={<div className='loadingStuff' />}
+            containerElement={<div className='loadingStuff' />}
+            mapElement={<div className='loadingStuff' />}
+            lat={points.lat}
+            lng={points.lng}
+            state={markerState}
+          />
+          <div id='info'>
+            <h1 id='lat'>?</h1>
+            <h1 id='lng'>?</h1>
+            <button onClick={() => newArea()} id='submitButton'>Submit</button>
+          </div>
+          <div id='infoshown'>
+            <h1 id='totalDifference'>Total difference : </h1>
           </div>
         </div>
       </div>
-
-
     </div>
   );
 }
